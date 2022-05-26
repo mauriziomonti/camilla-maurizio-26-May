7161486e68e35dde41563f3e60368d16039f3c73
mauriziomonti <mauriziomonti@hotmail.com>
2022-05-26 12:30:36 +0100

    first commit

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-// Create a folder
+// Create a folder √
 
 // Initialize an node/npm project
 
-// install dotenv as a dev dependency
+// install dotenv as a dev dependency √
 
-// Create some environment variables and put them in a .env
+// Create some environment variables and put them in a .env √
 
 // Read them in to the file we are working with
 
 // Checked that they were there! Alert if not. 
 
 // Use them
+
+import 'dotenv/config';
+import express from "express";
+
+console.log(process.env.PORT);
+console.log(process.env.PASSWORD);
