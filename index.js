c915c80a0b073cf34aa266c1f0a6e04960a64b0a
Maurizio Monti <mauriziomonti@hotmail.com>
2022-05-26 11:52:21 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// Create a folder
+
+// Initialize an node/npm project
+
+// install dotenv as a dev dependency
+
+// Create some environment variables and put them in a .env
+
+// Read them in to the file we are working with
+
+// Checked that they were there! Alert if not. 
+
+// Use them
